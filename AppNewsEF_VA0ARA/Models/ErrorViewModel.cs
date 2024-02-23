836d8ecbcb58f529986e8dac5ece02daafa6ce60
vahid ara <vahidaara@gmail.com>
2024-02-23 09:28:52 +0330

    Create Project

---
+++ AppNewsEF_VA0ARA/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AppNewsEF_VA0ARA.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
